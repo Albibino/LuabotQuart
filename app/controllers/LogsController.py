f807c0db8c76e84a3ef27cb7e4d5ab0c6981da4d
William Wollert <williamwollert@gmail.com>
2025-05-08 19:13:17 -0300

    v1.2 - merge feito

--- app/controllers/LogsController.py
+++ app/controllers/LogsController.py
@@ -8,10 +8,5 @@ async def get_logs():
     async with async_session_factory() as session:
         result = await session.execute(select(Log))
         logs = result.scalars().all()
-<<<<<<< HEAD
-        return jsonify([log.to_dict() for log in logs])
-=======
         return jsonify([log.to_dict() for log in logs])
 
-    
->>>>>>> 43a83a2d35896ed7055314e6df7ed9d9c32452a4
